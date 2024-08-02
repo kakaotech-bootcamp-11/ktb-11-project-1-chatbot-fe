432d9e8ab806654f3f3ca90cd80aa5979594c672
Kongji82 <Kongji4092@gmail.com>
2024-08-02 17:08:12 +0900

    Feature/sidebar (#6): 채팅 메시지 전송 기능
    * [feat] 사이드바 채팅 제목 fetch기능
    
    * [feat] 채팅 api 호출
    
    * [feat] 새로운 채팅시 redirect
    
    - 새로운 채팅시 createNewChat()
    - 새로운 채팅id로 redirect
    
    * [refactor] chaId type string -> number
    
    * [feature] 채팅 메시지 전송 기능

--- src/app/(home)/chat/[chatId]/components/Chat.tsx
+++ src/app/(home)/chat/[chatId]/components/Chat.tsx
@@ -1,62 +1,70 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+"use client"
+
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import ReactMarkdown from "react-markdown";
 
-import { messages } from "../dummyMessages";
-import { CodeBlock } from "./CodeBlock";
+import {CodeBlock} from "./CodeBlock";
+import {ChatContent, useChatHistoryQuery} from "@/app/(home)/hooks/useChatQuery";
+import {usePathname} from "next/navigation";
 
 export default function Chat() {
-  return (
-    <div className="flex h-full p-4 overflow-auto">
-      <div className="space-y-4">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`flex items-start gap-4 ${
-              message.sender === "user" ? "justify-end" : ""
-            }`}
-          >
-            {message.sender === "bot" && (
-              <Avatar className="flex-shrink-0 w-8 h-8 border">
-                <AvatarFallback>JD</AvatarFallback>
-              </Avatar>
-            )}
-            <div
-              className={`rounded-lg p-4 max-w-[75%] ${
-                message.sender === "user"
-                  ? "bg-primary text-primary-foreground"
-                  : "bg-muted text-muted-foreground text-[#0E1E46]"
-              }`}
-            >
-              <ReactMarkdown
-                className="prose"
-                components={{
-                  code({ node, className, children, ...props }) {
-                    const match = /language-(\w+)/.exec(className || "");
-                    return match ? (
-                      <CodeBlock
-                        language={match[1]}
-                        value={String(children).replace(/\n$/, "")}
-                        {...props}
-                      />
-                    ) : (
-                      <code className={className} {...props}>
-                        {children}
-                      </code>
-                    );
-                  },
-                }}
-              >
-                {message.content}
-              </ReactMarkdown>
+    const pathname = usePathname();
+    const nowChatId = pathname.match(/\/chat\/(\d+)/)!;
+    const {data, error, isLoading} = useChatHistoryQuery(parseInt(nowChatId[1]));
+    const chatMessages = data ?? [];
+
+    return (
+        <div className="flex h-full p-4 overflow-auto">
+            <div className="space-y-4">
+                {chatMessages.map((message, index) => (
+                    <div
+                        key={index}
+                        className={`flex items-start gap-4 ${
+                            message.isUser ? "justify-end" : ""
+                        }`}
+                    >
+                        {!message.isUser && (
+                            <Avatar className="flex-shrink-0 w-8 h-8 border">
+                                <AvatarFallback>JD</AvatarFallback>
+                            </Avatar>
+                        )}
+                        <div
+                            className={`rounded-lg p-4 max-w-[75%] ${
+                                message.isUser
+                                    ? "bg-primary text-primary-foreground"
+                                    : "bg-muted text-muted-foreground text-[#0E1E46]"
+                            }`}
+                        >
+                            <ReactMarkdown
+                                className="prose"
+                                components={{
+                                    code({node, className, children, ...props}) {
+                                        const match = /language-(\w+)/.exec(className || "");
+                                        return match ? (
+                                            <CodeBlock
+                                                language={match[1]}
+                                                value={String(children).replace(/\n$/, "")}
+                                                {...props}
+                                            />
+                                        ) : (
+                                            <code className={className} {...props}>
+                                                {children}
+                                            </code>
+                                        );
+                                    },
+                                }}
+                            >
+                                {message.content}
+                            </ReactMarkdown>
+                        </div>
+                        {message.isUser && (
+                            <Avatar className="flex-shrink-0 w-8 h-8 border">
+                                <AvatarFallback>CB</AvatarFallback>
+                            </Avatar>
+                        )}
+                    </div>
+                ))}
             </div>
-            {message.sender === "user" && (
-              <Avatar className="flex-shrink-0 w-8 h-8 border">
-                <AvatarFallback>CB</AvatarFallback>
-              </Avatar>
-            )}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
--- src/app/(home)/components/MessageInput.tsx
+++ src/app/(home)/components/MessageInput.tsx
@@ -1,46 +1,84 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { SendHorizontal } from "lucide-react";
-import { useEffect, useState } from "react";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {SendHorizontal} from "lucide-react";
+import {useEffect, useState} from "react";
+import {ChatContent, createNewChat, sendChatMessage} from "@/app/(home)/hooks/useChatQuery";
+import {usePathname, useRouter} from "next/navigation";
+import {useQueryClient} from "@tanstack/react-query";
 
 export default function MessageInput() {
-  const [inputValue, setInputValue] = useState("");
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+    const [inputValue, setInputValue] = useState("");
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+    const [chatId, setCurrentChatId] = useState<number|null>(null);
+    const queryClient = useQueryClient();
+    const pathname = usePathname();
+    const router = useRouter();
 
-  useEffect(() => {
-    setIsButtonDisabled(inputValue.trim() === "");
-  }, [inputValue]);
+    useEffect(() => {
+        const chatIdMatch = pathname.match(/\/chat\/(\d+)/);
+        if (chatIdMatch) {
+            setCurrentChatId(parseInt(chatIdMatch[1]));
+        } else {
+            setCurrentChatId(null);
+        }
+        setIsButtonDisabled(inputValue.trim() === "");
+    }, [inputValue]);
 
-  const handleSubmit = () => {
-    if (inputValue.trim() !== "") {
-      console.log("Message sent:", inputValue);
-      setInputValue(""); // 입력 필드 초기화
-    }
-  };
+    const handleSubmit = async () => {
+        if (inputValue.trim() !== "") {
+            if (chatId === null) {
+                const createdChat = await createNewChat(inputValue);
+                router.push(`/chat/${createdChat.chatId}`);
+                await queryClient.invalidateQueries({
+                    queryKey: ["getTitles"]
+                })
+            } else {
+                queryClient.setQueryData(["chatHistory", chatId], (prev: ChatContent[]) => {
+                    const newChat = {
+                        chatMessageId: prev[prev.length - 1].chatMessageId + 1,
+                        content: inputValue,
+                        isUser: true,
+                    };
+                    return [...prev, newChat];
+                });
 
-  return (
-    <div className="relative flex h-[80px] justify-center items-center p-2">
-      <Input
-        className="flex h-16 p-6 rounded-full "
-        placeholder="물어보고 싶은 질문을 입력해주세요!"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            handleSubmit();
-          }
-        }}
-      />
-      <Button
-        onClick={handleSubmit}
-        className="absolute right-8 rounded-full bg-[#0E1E46] hover:bg-gray-600"
-        variant="ghost"
-        disabled={isButtonDisabled}
-      >
-        <SendHorizontal className="text-white" />
-      </Button>
-    </div>
-  );
+                const data: ChatContent = await sendChatMessage(chatId, inputValue);
+                queryClient.setQueryData(["chatHistory", chatId], (prev: ChatContent[]) => {
+                    const newChat = {
+                        chatMessageId: prev[prev.length - 1].chatMessageId + 1,
+                        content: data.content,
+                        isUser: false,
+                    };
+                    return [...prev, newChat];
+                });
+            }
+            setInputValue(""); // 입력 필드 초기화
+        }
+    };
+
+    return (
+        <div className="relative flex h-[80px] justify-center items-center p-2">
+            <Input
+                className="flex h-16 p-6 rounded-full "
+                placeholder="물어보고 싶은 질문을 입력해주세요!"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        handleSubmit();
+                    }
+                }}
+            />
+            <Button
+                onClick={handleSubmit}
+                className="absolute right-8 rounded-full bg-[#0E1E46] hover:bg-gray-600"
+                variant="ghost"
+                disabled={isButtonDisabled}
+            >
+                <SendHorizontal className="text-white"/>
+            </Button>
+        </div>
+    );
 }
