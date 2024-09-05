472e2defb61823bb72a835e66728b8d8f52e6677
speardragon <rkdckdfyyd@naver.com>
2024-09-05 13:46:01 +0900

    hotfix: fix miss import statement

--- src/app/(home)/hooks/useDeleteChatMutation.ts
+++ src/app/(home)/hooks/useDeleteChatMutation.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
-import { AIResponse } from "./useCreateNewChatMutation";
+import { AIResponse } from "./useSendChatStreamMutation";
 
 export function useDeleteChatMutation() {
   const queryClient = useQueryClient();
