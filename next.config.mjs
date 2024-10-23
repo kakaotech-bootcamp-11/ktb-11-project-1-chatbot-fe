a1dbe2681fd9aada18f39f0796683bb63315b63f
speardragon <rkdckdfyyd@naver.com>
2024-10-23 14:49:11 +0900

    chore: 패키지 설치 및 next 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "img1.kakaocdn.net",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   async rewrites() {
